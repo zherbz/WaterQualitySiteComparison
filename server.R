ec28b47f0e4c9e73ae63f95d31650dd4256c784a
zherbz <44210203+zherbz@users.noreply.github.com>
2018-11-20 12:37:43 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -38,7 +38,7 @@ shinyServer(function(input, output) {
       
       geom_smooth(data = plotdata2, aes(x=plotdata2$time, y=plotdata2[,input$param3],color='blue')) +
       
-      labs(title = "Site Comparison Plot", x = "Time", y = input$param3, color = "Legend Title\n") +
+      labs(title = "Site Comparison Plot", x = "Time", y = input$param3, color = "Legend\n") +
       
       scale_color_manual(labels = c(sitename_1300[1], sitename_900[1]), values = c("red", "blue")) +
       
@@ -110,7 +110,8 @@ shinyServer(function(input, output) {
     plotdata3 <- subset(df, time >= input$dates[1] & time <= input$dates[2])
     
     ggplot((data = plotdata3), aes(x = plotdata3$SiteName, y = plotdata3[,input$param3], fill = plotdata3$SiteName)) +
-      geom_boxplot(outlier.colour="black", outlier.shape = 8, outlier.size = 4) + xlab("Sites") + ylab(input$param3)
+      geom_boxplot(outlier.colour="black", outlier.shape = 8, outlier.size = 4) + xlab("Sites") + ylab(input$param3) +
+      scale_fill_discrete(name = "Legend\n")
     
   })
   
